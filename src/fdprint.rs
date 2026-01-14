6577a6ed8768941e0cb15de445214837f8deb863
Alex Riesen <raa.lkml+github@gmail.com>
2026-01-14 15:59:41 +0100

    Cleanup: use libc::c_int instead of i32 for consistency

--- src/fdprint.rs
+++ src/fdprint.rs
@@ -4,7 +4,7 @@ extern crate libc;
 use misc::{u64toa, u32toa};
 
 #[cold]
-pub fn fdput_c_str(fd: i32, arg: *const libc::c_char) {
+pub fn fdput_c_str(fd: libc::c_int, arg: *const libc::c_char) {
     if !arg.is_null() {
         unsafe {
             libc::write(fd, arg as *const libc::c_void, libc::strlen(arg));
@@ -12,31 +12,33 @@ pub fn fdput_c_str(fd: i32, arg: *const libc::c_char) {
     }
 }
 
-pub fn fdput_bytes(fd: i32, arg: &[u8]) {
+pub fn fdput_bytes(fd: libc::c_int, arg: &[u8]) {
     unsafe {
         libc::write(fd, arg.as_ptr() as *const libc::c_void, arg.len());
     }
 }
 
-pub fn fdput_c_chars(fd: i32, arg: &[libc::c_char]) {
+pub fn fdput_c_chars(fd: libc::c_int, arg: &[libc::c_char]) {
     unsafe {
         libc::write(fd, arg.as_ptr() as *const libc::c_void, arg.len());
     }
 }
 
-pub trait FdPuts { fn fdputs(self, fd: i32); }
+pub trait FdPuts {
+    fn fdputs(self, fd: libc::c_int);
+}
 
 impl FdPuts for &str {
-    fn fdputs(self, fd: i32) { fdput_bytes(fd, self.as_bytes()) }
+    fn fdputs(self, fd: libc::c_int) { fdput_bytes(fd, self.as_bytes()) }
 }
 impl FdPuts for &[u8] {
-    fn fdputs(self, fd: i32) { fdput_bytes(fd, self) }
+    fn fdputs(self, fd: libc::c_int) { fdput_bytes(fd, self) }
 }
 impl FdPuts for &[libc::c_char] {
-    fn fdputs(self, fd: i32) { fdput_c_chars(fd, self) }
+    fn fdputs(self, fd: libc::c_int) { fdput_c_chars(fd, self) }
 }
 impl FdPuts for *const libc::c_char {
-    fn fdputs(self, fd: i32) {
+    fn fdputs(self, fd: libc::c_int) {
         if self.is_null() {
             fdput_bytes(fd, b"(null)")
         } else {
@@ -45,7 +47,7 @@ impl FdPuts for *const libc::c_char {
     }
 }
 impl FdPuts for i32 {
-    fn fdputs(self, fd: i32) {
+    fn fdputs(self, fd: libc::c_int) {
         let v;
         if self < 0 {
             v = -self;
@@ -57,12 +59,12 @@ impl FdPuts for i32 {
     }
 }
 impl FdPuts for u32 {
-    fn fdputs(self, fd: i32) {
+    fn fdputs(self, fd: libc::c_int) {
         fdput_bytes(fd, u32toa(self, &mut [ 0u8; 10 ]))
     }
 }
 impl FdPuts for i64 {
-    fn fdputs(self, fd: i32) {
+    fn fdputs(self, fd: libc::c_int) {
         let v;
         if self < 0 {
             v = -self;
@@ -74,12 +76,12 @@ impl FdPuts for i64 {
     }
 }
 impl FdPuts for u64 {
-    fn fdputs(self, fd: i32) {
+    fn fdputs(self, fd: libc::c_int) {
         fdput_bytes(fd, u64toa(self, &mut [ 0u8; 24 ]))
     }
 }
 impl FdPuts for isize {
-    fn fdputs(self, fd: i32) {
+    fn fdputs(self, fd: libc::c_int) {
         let v;
         if self < 0 {
             v = -self;
@@ -91,7 +93,7 @@ impl FdPuts for isize {
     }
 }
 impl FdPuts for usize {
-    fn fdputs(self, fd: i32) {
+    fn fdputs(self, fd: libc::c_int) {
         let mut buf = [ 0u8; 20 ];
         static HEX: &[u8; 16] = b"0123456789abcdef";
         let mut u = self;
@@ -107,10 +109,10 @@ impl FdPuts for usize {
     }
 }
 impl FdPuts for u8 {
-    fn fdputs(self, fd: i32) { fdput_bytes(fd, &[ self ]) }
+    fn fdputs(self, fd: libc::c_int) { fdput_bytes(fd, &[ self ]) }
 }
 impl FdPuts for i8 {
-    fn fdputs(self, fd: i32) { fdput_bytes(fd, &[ self as u8 ]) }
+    fn fdputs(self, fd: libc::c_int) { fdput_bytes(fd, &[ self as u8 ]) }
 }
 
 #[macro_export]
